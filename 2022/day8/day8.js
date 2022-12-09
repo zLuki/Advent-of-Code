13878d91f4ea257278043e6b7f26d0f706af583d
Lukas Schatzer <Schatzer.Lukas@gmail.com>
2022-12-09 19:15:08 +0100

    completed day8

---
+++ 2022/day8/day8.js
@@ -0,0 +1,63 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("input.txt", "utf-8").split("\r\n").map(e => e.split("").map(e => parseInt(e)));
+
+
+let bestAllamount = 0;
+for (let i = 0; i < input.length; i++) {
+    for (let x = 0; x < input[i].length; x++) {
+        
+        let allAmount = 1;
+
+        // look up
+        let amount = 0;
+        for (let j = i-1; j >= 0; j--) {
+            amount++;
+            if (input[j][x] >= input[i][x]) {
+                visible = false;
+                break;
+            }
+        }
+        allAmount *= amount;
+
+        // look down
+        amount = 0;
+        for (let j = i+1; j < input.length; j++) {
+            amount++;
+            if (input[j][x] >= input[i][x]) {
+                visible = false;
+                break;
+            }
+        }
+        allAmount *= amount;
+
+        // look left
+        amount = 0;
+        for (let j = x-1; j >= 0; j--) {
+            amount++;
+            if (input[i][j] >= input[i][x]) {
+                visible = false;
+                break;
+            }
+        }
+        allAmount *= amount;
+
+        // look right
+        amount = 0;
+        for (let j = x+1; j < input[i].length; j++) {
+            amount++;
+            if (input[i][j] >= input[i][x]) {
+                visible = false;
+                break;
+            }
+        }
+        allAmount *= amount;
+
+        if (allAmount > bestAllamount) {
+            bestAllamount = allAmount;
+        }
+
+    }
+}
+
+console.log(bestAllamount);
